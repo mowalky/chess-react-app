5df58bb86aca2b78184ccf73f512d031c5920f42
Mowalky <dev@mowalky.com>
2022-09-26 22:52:19 -0500

    board setup

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -5,5 +5,18 @@ const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8];
 
 export default function Chessboard() {
-  return <div id="chessboard">chessboard</div>;
+  const board: any = [];
+
+  for (let i = 0; i < horizontalAxis.length; i++) {
+    for (let j = 0; j < verticalAxis.length; j++) {
+      board.push(
+        <span>
+          {horizontalAxis[i]}
+          {verticalAxis[j]}
+        </span>
+      );
+    }
+  }
+
+  return <div id="chessboard">{board}</div>;
 }
