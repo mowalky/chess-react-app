88b2040b4fd4e9b4ed21c26d36852ae3e01949cd
Mowalky <dev@mowalky.com>
2022-09-26 22:40:42 -0500

    board setup and color

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Chessboard.css";
 
+const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8];
+
 export default function Chessboard() {
-  return <div>chessboard</div>;
+  return <div id="chessboard">chessboard</div>;
 }
