4314deff429e497bca857d82c894dc2df0ae5d2d
mowalky <mowalky@outlook.com>
2022-09-29 22:24:40 -0500

    updated piece component

--- src/components/Pieces/Piece.tsx
+++ src/components/Pieces/Piece.tsx
@@ -2,7 +2,7 @@ const ChessPiece = ({
   piece = "pawn",
   color = "w",
 }: {
-  piece: "pawn" | "king" | "queen" | "rook" | "knight";
+  piece: "pawn" | "king" | "queen" | "rook" | "knight" | "bishop" | "rook";
   color: "b" | "w";
 }) => {
   return <img alt="pawn" src={`/assets/images/${piece}_${color}.png`} />;
