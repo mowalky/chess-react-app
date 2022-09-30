227bfdc4913913e3663073f6792c369dde242240
mowalky <mowalky@outlook.com>
2022-09-29 23:36:21 -0500

    board piece setup

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -4,16 +4,24 @@ import Tilt from "../Tile/Tile";
 const horizontalSquares = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const verticalSquares = [1, 2, 3, 4, 5, 6, 7, 8];
 
-const topSetup = [
-  "rook",
-  "knight",
-  "bishop",
-  "king",
-  "queen",
-  "bishop",
-  "knight",
-  "rook",
-];
+const topSetup: any = {
+  a1: "rook",
+  b1: "bishop",
+  c1: "knight",
+  d1: "king",
+  e1: "queen",
+  f1: "knight",
+  g1: "bishop",
+  h1: "rook",
+  a2: "pawn",
+  b2: "pawn",
+  c2: "pawn",
+  d2: "pawn",
+  e2: "pawn",
+  f2: "pawn",
+  g2: "pawn",
+  h2: "pawn",
+};
 
 export default function Chessboard() {
   const board: any = [];
@@ -24,7 +32,7 @@ export default function Chessboard() {
       const square = horizontalSquares[j] + count;
       board.push(
         <Tilt
-          piece={topSetup[j]}
+          piece={topSetup[square]}
           key={square}
           tile={square}
           tileNumber={tileNumber}
--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -6,16 +6,18 @@ const Tile = ({
   tile,
   tileNumber,
   piece,
+  color,
 }: {
   tile: string;
   tileNumber: number;
   piece?: any;
+  color?: any;
 }) => {
   return (
     <span
       className={`tile ${tileNumber % 2 === 0 ? "black-tile" : "white-tile"}`}
     >
-      {piece && <ChessPiece piece={piece} color="b" />}
+      {piece && <ChessPiece piece={piece} color={color} />}
     </span>
   );
 };
