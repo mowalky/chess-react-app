c6c289aa34c1b6b9895228fc3b4fe5fd9411f97e
mowalky <mowalky@outlook.com>
2022-10-04 22:17:16 -0500

    refactored bishop moves

--- src/moves/bishop.ts
+++ src/moves/bishop.ts
@@ -5,21 +5,14 @@ const bishopMoves = (x: number, y: string, color: string) => {
   let moves: any = [];
   let curLetter = y;
   let preLetter = y;
-  let curLetter2 = y;
-  let preLetter2 = y;
-  values.forEach((v, i) => {
+  values.forEach((_, i) => {
     moves.push(`${nextLetter(curLetter)}${x + (i + 1)}`);
+    moves.push(`${nextLetter(curLetter)}${x - (i + 1)}`);
     curLetter = nextLetter(curLetter);
     moves.push(`${prevLetter(preLetter)}${x + (i + 1)}`);
+    moves.push(`${prevLetter(preLetter)}${x - (i + 1)}`);
     preLetter = prevLetter(preLetter);
-
-    moves.push(`${nextLetter(curLetter2)}${x - (i + 1)}`);
-    curLetter2 = nextLetter(curLetter2);
-    moves.push(`${prevLetter(preLetter2)}${x - (i + 1)}`);
-    preLetter2 = prevLetter(preLetter2);
   });
-
-  //let moves = [`${nextLetter(y)}${x + 1}`, `${prevLetter(y)}${x + 1}`];
   return moves;
 };
 
