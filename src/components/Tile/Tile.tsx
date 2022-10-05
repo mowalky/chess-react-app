9e8af86bd9ad6e6f710e87fe4ec945d05dd40314
mowalky <mowalky@outlook.com>
2022-10-04 21:49:12 -0500

    rook moves

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -3,6 +3,20 @@ import { useState } from "react";
 
 import ChessPiece from "../Pieces/Piece";
 
+const rookMoves = (x: number, y: string, color: string) => {
+  const values = Array.from({ length: 8 }, (_, i) => i + 1);
+  let moves = values.map((a) => `${y}${color === "b" ? x - a : x + a}`);
+  let curLetter = y;
+  let preletter = y;
+  values.forEach((v) => {
+    moves.push(`${nextLetter(curLetter)}${x}`);
+    curLetter = nextLetter(curLetter);
+    moves.push(`${prevLetter(preletter)}${x}`);
+    preletter = prevLetter(preletter);
+  });
+
+  return moves;
+};
 interface validMoves {
   boardstate?: [];
   square: string;
@@ -51,17 +65,7 @@ const availableMoves = ({ boardstate, square, piece, color }: validMoves) => {
 
     case "rook":
       console.log(`(${color})rook moves from ${square}`);
-      const values = Array.from({ length: 8 }, (_, i) => i + 1);
-      moves = values.map((a) => `${y}${color === "b" ? x - a : x + a}`);
-      let curLetter = y;
-      let preletter = y;
-      values.forEach((v) => {
-        moves.push(`${nextLetter(curLetter)}${x}`);
-        curLetter = nextLetter(curLetter);
-        moves.push(`${prevLetter(preletter)}${x}`);
-        preletter = prevLetter(preletter);
-      });
-
+      moves = rookMoves(x, y, color);
       break;
   }
 
