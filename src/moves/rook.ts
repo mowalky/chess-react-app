49fe4bda455f78e3a9cb8369e37f1ddb619e6fd9
mowalky <mowalky@outlook.com>
2022-10-05 22:49:11 -0500

    updated rook moves

--- src/moves/rook.ts
+++ src/moves/rook.ts
@@ -2,12 +2,13 @@ import { nextLetter, prevLetter } from "./utils";
 
 const rookMoves = (x: number, y: string, color: string) => {
   const values = Array.from({ length: 8 }, (_, i) => i + 1);
-  let moves = values.map((a) => `${y}${color === "b" ? x - a : x + a}`);
+  let moves: any = [];
   let curLetter = y;
   let preletter = y;
 
   values.forEach((v, i) => {
     console.log(x);
+    moves.push(`${y}${x + v}`);
     moves.push(`${nextLetter(curLetter)}${x}`);
     moves.push(`${y}${x - (i + 1)}`);
     moves.push(`${prevLetter(preletter)}${x}`);
