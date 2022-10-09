785d33705d6dff5ae8a3365cea13364964fbf1dc
mowalky <mowalky@outlook.com>
2022-10-08 20:46:39 -0500

    updated queen vision

--- src/moves/rook.ts
+++ src/moves/rook.ts
@@ -6,17 +6,23 @@ const rookMoves = (x: number, y: string, color: string, board?: any) => {
   let curLetter = y;
   let preletter = y;
 
+  // piece vision
   let stopLeft = false;
+  let stopRight = false;
+  let stopUp = false;
+  let stopDown = false;
   let stopLeftTopDiag = false;
   let stopRightTopDiag = false;
   let stopLeftBotDiag = false;
   let stopRightBotDiag = false;
 
   values.forEach((v, i) => {
-    moves.push(`${y}${x + v}`);
+    if (!stopUp) {
+      moves.push(`${y}${x + v}`);
+      if (board[`${y}${x + v}`]?.p) stopUp = true;
+    }
     moves.push(`${nextLetter(curLetter)}${x}`);
     moves.push(`${y}${x - (i + 1)}`);
-    console.log(`${prevLetter(preletter)}${x}`);
 
     if (!stopLeft) {
       moves.push(`${prevLetter(preletter)}${x}`);
