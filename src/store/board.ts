cb650c0f829617eaa13b306e0bc74089c2b548d3
mowalky <mowalky@outlook.com>
2022-10-21 22:37:08 -0500

    updated store

--- src/store/board.ts
+++ src/store/board.ts
@@ -2,6 +2,10 @@ import create from "zustand";
 
 const useStore = create((set) => ({
   board: {},
+  wCaptured: [],
+  wMoveHistory: [],
+  bCaptured: [],
+  bMoveHistory: [],
 }));
 
 export const useBoardStore = useStore;
