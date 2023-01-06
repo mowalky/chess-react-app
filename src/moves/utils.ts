b08370aa4a88af219cc89d938b3c113c58f21f80
mowalky <mowalky@outlook.com>
2023-01-05 22:40:10 -0600

    added object to fen util function

--- src/moves/utils.ts
+++ src/moves/utils.ts
@@ -62,3 +62,91 @@ export function fen(fen: string) {
 
   // convert back
 }
+
+function fenToObject(fen: string): { [key: string]: { p: string; c: string } } {
+  const rows = fen.split(" ")[0].split("/");
+  const result: { [key: string]: { p: string; c: string } } = {};
+
+  let rowIndex = 8;
+  for (const row of rows) {
+    let colIndex = "a";
+    for (const cell of row) {
+      if (Number.isInteger(parseInt(cell))) {
+        colIndex = String.fromCharCode(colIndex.charCodeAt(0) + parseInt(cell));
+      } else {
+        result[colIndex + rowIndex] = {
+          p: cellToPiece(cell),
+          c: cellToColor(cell),
+        };
+        colIndex = String.fromCharCode(colIndex.charCodeAt(0) + 1);
+      }
+    }
+    rowIndex--;
+  }
+
+  return result;
+}
+
+function cellToPiece(cell: string): string {
+  const pieceMap: any = {
+    P: "pawn",
+    N: "knight",
+    B: "bishop",
+    R: "rook",
+    Q: "queen",
+    K: "king",
+  };
+  return pieceMap[cell.toUpperCase()] || "";
+}
+
+function cellToColor(cell: string): "w" | "b" {
+  return cell === cell.toUpperCase() ? "w" : "b";
+}
+
+export function objectToFen(obj: {
+  [key: string]: { p: string; c: "w" | "b" };
+}): string {
+  let fen = "";
+  for (let row = 8; row > 0; row--) {
+    let emptySquares = 0;
+    for (
+      let col = "a";
+      col <= "h";
+      col = String.fromCharCode(col.charCodeAt(0) + 1)
+    ) {
+      const cell: { p: string; c: "w" | "b" } = obj[col + row];
+      if (cell) {
+        if (emptySquares) {
+          fen += emptySquares;
+          emptySquares = 0;
+        }
+        fen += pieceToCell(cell.p, cell.c);
+      } else {
+        emptySquares++;
+      }
+    }
+    if (emptySquares) {
+      fen += emptySquares;
+    }
+    if (row > 1) {
+      fen += "/";
+    }
+  }
+  return fen;
+}
+
+function pieceToCell(piece: string, color: "w" | "b"): string {
+  const colorMap = {
+    b: "",
+    w: "",
+  };
+  const pieceMap = {
+    pawn: "P",
+    knight: "N",
+    bishop: "B",
+    rook: "R",
+    queen: "Q",
+    king: "K",
+  } as Record<string, string>;
+  return colorMap[color] + pieceMap[piece];
+}
