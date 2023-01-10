2be890b7773838b885dbefd7435a2142afcff16d
mowalky <mowalky@outlook.com>
2023-01-09 23:12:09 -0600

    added king in check

--- src/moves/king.ts
+++ src/moves/king.ts
@@ -13,4 +13,25 @@ const kingMoves = (x: number, y: string, color: string) => {
   ];
 };
 
+const isKingInCheck = (x: number, y: string, color: string, board: Board) => {
+  const kingMoves = [
+    `${y}${color === "b" ? x - 1 : x + 1}`,
+    `${prevLetter(y)}${x + 1}`,
+    `${nextLetter(y)}${x + 1}`,
+    `${nextLetter(y)}${x}`,
+    `${prevLetter(y)}${x}`,
+    `${prevLetter(y)}${x - 1}`,
+    `${nextLetter(y)}${x - 1}`,
+    `${y}${color === "b" ? x + 1 : x - 1}`,
+  ];
+
+  for (const move of kingMoves) {
+    if (board[move] && board[move].color !== color) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
 export default kingMoves;
